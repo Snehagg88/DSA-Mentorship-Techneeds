089be0870bb8f76d6e147845cb9b760d32eac60a
Snehagg88 <144485274+Snehagg88@users.noreply.github.com>
2023-10-03 22:31:52 +0530

    Update A1_Q1.cpp

--- A1_Q1.cpp
+++ A1_Q1.cpp
@@ -1,1 +1,22 @@
 
+#include<iostream> 
+ using namespace std; 
+ int main(){ 
+     int n=5; 
+     for(int i=0;i<n;i++){ 
+         for(int j=0;j<n;j++){ 
+             if(i==0||i==n-1){ 
+                 cout<<"*"; 
+             } 
+             else if(i+j==n-1){ 
+                 cout<<"*"; 
+  
+             } 
+             else{ 
+                 cout<<" ";     
+             }     
+         } 
+         cout<<endl; 
+     }     
+     return 0; 
+ }
