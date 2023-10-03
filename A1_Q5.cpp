1495209b242372450d45d3169ab336ef1936e003
Snehagg88 <144485274+Snehagg88@users.noreply.github.com>
2023-10-04 02:37:55 +0530

    Create A1_Q5.cpp

---
+++ A1_Q5.cpp
@@ -0,0 +1,14 @@
+#include<iostream> 
+ using namespace std; 
+ int main(){ 
+     int n; 
+     cin>>n; 
+     int digits=0; 
+     while(n>0){ 
+         digits++; 
+         n=n/10; 
+     } 
+     cout<<digits; 
+     return 0; 
+  
+ }
