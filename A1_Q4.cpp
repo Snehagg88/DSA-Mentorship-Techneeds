59898ebed9c20abe07dc3f97eb3f9f83f8079263
Snehagg88 <144485274+Snehagg88@users.noreply.github.com>
2023-10-06 02:31:14 +0530

    Update A1_Q4.cpp

--- A1_Q4.cpp
+++ A1_Q4.cpp
@@ -1,1 +1,21 @@
+#include<iostream>
+using namespace std ;
+int main(){
+  int n;
+cout<<"Enter value of n";
+cin>>n;
+  while(n){
 
+    for(i=2;i<p;i++){
+      if(p%i==0);
+      break;
+    }
+    if (i==p)
+{
+cout<<p<<" ";
+n--;
+}
+p++; 
+  }
+return 0;
+}
