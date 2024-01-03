71849b30892e3a9905a3c40c1fa3b449583b75e3
Snehagg88 <144485274+Snehagg88@users.noreply.github.com>
2024-01-03 19:20:59 +0530

    Create A2_Q20.cpp

---
+++ A2_Q20.cpp
@@ -0,0 +1,20 @@
+#include<iostream> 
+ using namespace std; 
+ int main(){ 
+     int n; 
+     cin>>n; 
+     for(int i=1;i<=n;i++){ 
+         for(int j=1;j<=n;j++){ 
+             if(j==1||j==n){ 
+                 cout<<"*"; 
+             } 
+             else if((i>n/2 && i!=n)&&(i+j==n+1||j==i)){ 
+                 cout<<"*"; 
+             } 
+             else{ 
+                 cout<<" "; 
+             } 
+         } 
+         cout<<endl; 
+     } 
+ }
