0139d6566a1887d43afc7e415e6795d46ae3461b
Snehagg88 <144485274+Snehagg88@users.noreply.github.com>
2024-01-03 18:29:35 +0530

    Create A2_Q1.cpp

---
+++ A2_Q1.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+void print1(int n){
+    for(int i=0;i<n;i++){
+        for(int j=0;j<=i;j++){
+         cout<<"*"<<" ";
+        } cout<<endl;
+    }
+        }
+       int main(){
+           int n ;
+           cin>>n;
+           
+           print1(n);
+       }
